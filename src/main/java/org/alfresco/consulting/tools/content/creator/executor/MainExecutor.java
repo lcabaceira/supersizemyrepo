ebd76715fd19e99325f1935aa889fd82b0ed4f01
astrachan <alex.strachan@alfresco.com>
2014-08-26 21:37:04 +0100

    Altered MainExecutor for additional max_files_per_folder utilisation by the ssmr-ui

--- src/main/java/org/alfresco/consulting/tools/content/creator/executor/MainExecutor.java
+++ src/main/java/org/alfresco/consulting/tools/content/creator/executor/MainExecutor.java
@@ -4,10 +4,6 @@ package org.alfresco.consulting.tools.content.creator.executor;
 import org.alfresco.consulting.locator.PropertiesLocator;
 import org.alfresco.consulting.tools.content.creator.agents.*;
 
-import java.io.File;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -31,22 +27,22 @@ public class MainExecutor {
 	private static Boolean jpg = true;
 
 
-
+    // --astrachan - not called by the UI 
     public static void main(String[] args) {
-            // not called by the UI
-            System.out.println("### not called by UI");
+ 
+            System.out.println("### not called by the UI");
             props = PropertiesLocator.getProperties("super-size-my-repo.properties");
             num_Threads = props.getProperty("num_Threads");
             threadPoolSize = props.getProperty("threadPoolSize");
             files_deployment_location = props.getProperty("files_deployment_location");
             images_location = props.getProperty("images_location");
             max_files_per_folder = props.getProperty("max_files_per_folder");
-            //doWork(num_Threads, threadPoolSize, files_deployment_location, images_location,true, true, true, true, true);
+  
             doWorkWithMaxFiles(max_files_per_folder,num_Threads, threadPoolSize, files_deployment_location, images_location,true, true, true, true, true);
         }
 
 
-
+    // called by the UI
     public static void main(String[] args, Properties propsUI) {
     	
     	if (!propsUI.isEmpty()){
@@ -54,16 +50,13 @@ public class MainExecutor {
     	}
     	
    
-   if (args.length > 1){
-	// called by UI
-	   			for (String arg : args){
-	   				System.out.println("### " + arg);
-	   			}
-	   
-	   			num_Threads 				= 	args[0];
-	   			threadPoolSize				= 	args[1];
-	   			files_deployment_location 	= 	args[2];
-	   			images_location 			= 	args[3];
+    	if (args.length > 1){
+	
+	   			max_files_per_folder				= args[0];
+	   			num_Threads 						= 	args[1];
+	   			threadPoolSize						= 	args[2];
+	   			files_deployment_location 	= 	args[3];
+	   			images_location 					= 	args[4];
 	   			
 	   			if (!args[4].equals("true")){
 	   				pdf = false;
@@ -80,66 +73,22 @@ public class MainExecutor {
 	 			if (!args[8].equals("true")){
 	   				jpg = false;
 	   			}
-	   			
-	   			
-	   			System.out.println("### called by UI");
-	   			doWork(num_Threads, threadPoolSize, files_deployment_location, images_location, pdf, ppt, xls, doc, jpg);  
-	   
-   			} else {
-	  // not called by the UI 
-	   		System.out.println("### not called by UI");
-	   		props = PropertiesLocator.getProperties("super-size-my-repo.properties");
-	   		num_Threads = props.getProperty("num_Threads");
-	   		threadPoolSize = props.getProperty("threadPoolSize");
-	   		files_deployment_location = props.getProperty("files_deployment_location");
-	   		images_location = props.getProperty("images_location");
-
-
-
-	   		doWork(num_Threads, threadPoolSize, files_deployment_location, images_location,true, true, true, true, true);   
-      }
+	 			System.out.println("### called by UI cakey");
+	   			doWorkWithMaxFiles(max_files_per_folder,num_Threads, threadPoolSize, files_deployment_location, images_location, pdf, ppt, xls, doc, jpg);  
+   			} 
     }
-   
-    // popped this work into it's own method for now
-   public static void doWork(String num_Threads, String threadPoolSize, String deployPath, String images, Boolean pdf, Boolean ppt, Boolean xls, Boolean doc, Boolean jpg){
-	   
-	   			ExecutorService executor = Executors.newFixedThreadPool(Integer.valueOf(threadPoolSize));
-        
-	   				for (int i = 0; i < Integer.valueOf(num_Threads); i++) {
-
-                            Runnable workerppt = new MSPowerPointAgent(deployPath, images, properties);
-	   						Runnable workerPdf = new PdfAgent(deployPath, images, num_Threads, properties);
-	   						Runnable workerxls = new MSExcelAgent(deployPath, images, properties);
-	   						Runnable workerdoc = new MSWordAgent(deployPath, images, properties);
-	   						Runnable workerjpg = new JpgAgent(deployPath, images, properties);
-
-
-                            if (ppt) {executor.execute(workerppt);}
-	   						if (pdf) {executor.execute(workerPdf);}
-	   						if (xls) {executor.execute(workerxls);}
-	   						if (doc) {executor.execute(workerdoc);}
-	   						if (jpg) {executor.execute(workerjpg);}
-
-        }
-        // This will make the executor accept no new threads
-        // and finish all existing threads in the queue
-        executor.shutdown();
-        // Wait until all threads are finish
-
-        try {
-            executor.awaitTermination(500, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Finished all threads");
-    }
-
 
    public static void doWorkWithMaxFiles(String maxFiles, String num_Threads, String threadPoolSize, String deployPath, String images, Boolean pdf, Boolean ppt, Boolean xls, Boolean doc, Boolean jpg){
 
-        ExecutorService executor = Executors.newFixedThreadPool(Integer.valueOf(threadPoolSize));
+	   		System.out.println("### maxFiles: " + maxFiles);
+			System.out.println("### num_Threads: " + num_Threads);
+			System.out.println("### threadPoolSize: " + threadPoolSize);
+			System.out.println("### deployPath: " + deployPath);
+			System.out.println("### images: " + images);
+	
+			ExecutorService executor = Executors.newFixedThreadPool(Integer.valueOf(threadPoolSize));
 
-        for (int i = 0; i < Integer.valueOf(num_Threads); i++) {
+			for (int i = 0; i < Integer.valueOf(num_Threads); i++) {
 
             Runnable workerppt = new MSPowerPointAgent(maxFiles,deployPath, images, properties);
             Runnable workerPdf = new PdfAgent(maxFiles,deployPath, images, num_Threads, properties);
@@ -166,8 +115,5 @@ public class MainExecutor {
             e.printStackTrace();
         }
         System.out.println("Finished all threads");
-    }
-
-
-
+   }
 }
