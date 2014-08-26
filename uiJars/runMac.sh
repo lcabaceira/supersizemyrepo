748faad7d392c8fd78c9a9f0464c659eddcb4fbb
Luis Cabaceira <luis.cabaceira@alfresco.com>
2014-08-26 22:57:19 +0100

    adding ui jars for version 0.4 with max_files_per_folder

--- uiJars/runMac.sh
+++ uiJars/runMac.sh
@@ -1,1 +1,1 @@
-java -XstartOnFirstThread -jar ./ssmr-ui-0.0.2-osx-20140813.193121-1-jar-with-dependencies.jar 
+java -XstartOnFirstThread -jar ./ssmr-ui-0.0.4-osx-20140826.212214-1-jar-with-dependencies.jar 
